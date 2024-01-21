5b770654e3e426ad1aa70c25cc7cf0655971883b
reza <mardanireza58@gmail.com>
2024-01-21 19:54:44 +0330

    add: User Model

--- account_module/models.py
+++ account_module/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractUser):
+    GENDER_CHOISE = (
+        ('M', 'male'),
+        ('F', 'female'),
+    )
+    phone_number = models.CharField(max_length=11, null=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOISE, default='M')
+    date_of_birth = models.DateField()
+    state = models.CharField(max_length=250, null=True)
+    city = models.CharField(max_length=250, null=True)
+    city_address = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account_module.apps.AccountModuleConfig'
 ]
 
 MIDDLEWARE = [
@@ -115,7 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'account_module.User'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
