27643a7aed895d44ea29ffe47f526de7d6f2b7d4
reza <mardanireza58@gmail.com>
2024-01-22 08:24:36 +0330

    config product module

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'account_module.apps.AccountModuleConfig'
+    'account_module.apps.AccountModuleConfig',
+    'product_module.apps.ProductModuleConfig'
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('account_module.urls', namespace='account'))
+    path('account/', include('account_module.urls', namespace='account')),
+    path('product', include('product_module.urls', namespace='product'))
 ]
